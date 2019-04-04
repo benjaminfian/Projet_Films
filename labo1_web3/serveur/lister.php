c6f258e679f102bbc6fbe1d35778acc9a6e3bc99
benjaminfian <48064921+benjaminfian@users.noreply.github.com>
2019-04-03 23:01:29 -0400

    Create lister.php

---
+++ labo1_web3/serveur/lister.php
@@ -0,0 +1,119 @@
+<?php
+include '../include/header.php';
+require_once("../BD/connexion.inc.php");
+?>
+
+<div class="container">
+    
+    <h2>Liste des films</h2><br>
+    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#form_enreg">Ajouter</button>
+    
+    <!-- The Modal -->
+      <div class="modal fade" id="form_enreg">
+        <div class="modal-dialog">
+            <form id="enregForm" enctype="multipart/form-data" action="enregistrer.php" method="POST" onSubmit="return valider();">
+
+                <div class="modal-content">
+
+                    <!-- Modal Header -->
+                    <div class="modal-header">
+                      <h4 class="modal-title">Enregistrer un nouveau film</h4>
+                      <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+
+                    <!-- Modal body -->
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="titre" name="titre" placeholder="titre du film"><br>
+                            <input type="text" class="form-control" id="res" name="res" placeholder="nom du realisateur"><br>                        
+                            <select class="form-control" id="categ" name="categ">
+                                <option selected hidden>catégorie</option>
+                                <option>Action</option>                            
+                                <option>Drame</option>
+                                <option>Horreur</option>                            
+                                <option>Science-fiction</option>
+                            </select><br>
+                            <input type="number" class="form-control" id="duree" name="duree" placeholder="durée du film (min)" min="0"><br>
+                            <input type="number" class="form-control" id="prix" name="prix" placeholder="prix de la location" min="0"><br>
+                            <label for="pochette">image de la pochette : </label>
+                            <input type="file" class="form-control-file" id="pochette" name="pochette">
+                        </div>
+                    </div>
+
+                    <!-- Modal footer -->
+                    <div class="modal-footer">
+                      <button type="submit" class="btn btn-primary" >Enregistrer</button>
+                    </div>        
+                </div>				
+              </form>      
+        </div>
+      </div>
+    
+    
+<?php
+$tab=array();
+$dossier="../pochettes/";
+$rep='<div class="row">
+        <div class="col-sm-12 col-md-10 col-md-offset-1"><br>
+            <table class="table table-hover table-striped">
+                <thead>
+                    <tr>
+                        <th>Pochette</th>
+                        <th class="text-center">Titre</th>
+                        <th class="text-center">Réalisateur</th>
+                        <th class="text-center">Catégorie</th>
+                        <th class="text-center">Durée</th>
+                        <th class="text-center">Prix</th>
+                        <th class="text-left">Gestion</th>
+                        <th></th></tr>
+                </thead>
+                <tbody>';
+$requette="SELECT * FROM film";
+    try
+    {
+		 $stmt = $connexion->prepare($requette);
+		 $stmt->execute();
+		 while($ligne=$stmt->fetch(PDO::FETCH_OBJ))
+         {
+            $rep.='<tr>
+                    <td class="col-sm-1 col-md-1">
+                        <div class="media">
+                            <a class="thumbnail pull-left" href="#"> 
+                                <img class="img_crud" src="../pochettes/'.($ligne->image).'" alt="Film '.($ligne->categorie).'" title="image de la boite du film '.($ligne->titre).'">
+                            </a>                            
+                        </div>
+                    </td>
+                    <td class="col-sm-1 col-md-1 text-center"><strong>'.($ligne->titre).'</strong></td>
+                    <td class="col-sm-1 col-md-1 text-center"><strong>'.($ligne->realisateur).'</strong></td>
+                    <td class="col-sm-1 col-md-1 text-center"><strong>'.($ligne->categorie).'</strong></td>
+                    <td class="col-sm-1 col-md-1 text-center"><strong>'.($ligne->duree).' min</strong></td>
+                    <td class="col-sm-1 col-md-1 text-center"><strong>'.($ligne->prix).' $</strong></td>
+                    <td class="col-sm-1 col-md-1">                         
+                        <form id="formFiche" action="fiche.php" method="POST">
+                            <input type="hidden" name="idModifier" value="'.($ligne->idFilm).'">
+                            <input type="submit" class="btn btn-success" value="Modifier">
+                        </form>                                                
+                    </td>
+                    <td class="col-sm-1 col-md-1">
+                        <form action="enlever.php" method="POST">
+                            <input type="hidden" name="idSupprimer" value="'.($ligne->idFilm).'">
+                            <input type="submit" class="btn btn-danger" value="Supprimer">                            
+                        </form>
+                    </td>
+                </tr>';					 
+		 }
+	 }
+    catch (Exception $e)
+    {
+		echo "Probleme pour lister";
+	 }
+    finally 
+    {
+		$rep.='</tbody></table></div></div></div>';
+		unset($connexion);
+		unset($stmt);
+		echo $rep;        
+	 }  
+
+include '../include/footer.php';
+?>

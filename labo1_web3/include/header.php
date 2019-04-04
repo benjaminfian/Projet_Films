accdedabcaf8c313f221c30aaa83c6653ecd0166
benjaminfian <48064921+benjaminfian@users.noreply.github.com>
2019-04-03 23:01:00 -0400

    Create header.php

---
+++ labo1_web3/include/header.php
@@ -0,0 +1,18 @@
+<?php
+?>
+<html>
+<head>
+    <title>Films</title>    
+    <meta charset="utf-8">  
+	<script src="../js/jquery-3.3.1.min.js"></script>
+    <script src="../js/bootstrap.bundle.min.js"></script>
+	<script src="../js/bootstrap.min.js"></script>
+    <script src="../js/general.js"></script>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+	<link rel="stylesheet" href="../css/bootstrap-grid.min">
+	<link rel="stylesheet" href="../css/bootstrap-reboot.min">
+	<link rel="stylesheet" href="../css/monCSS.css">    
+</head>
+    
+<body>
+    <h1>HEADER</h1>

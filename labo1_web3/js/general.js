096a76c80288917165bee3b1577ab54d0d62f454
benjaminfian <48064921+benjaminfian@users.noreply.github.com>
2019-04-08 13:42:20 -0400

    Create general.js

---
+++ labo1_web3/js/general.js
@@ -0,0 +1,10 @@
+function deconnexion()
+{    
+    document.getElementById('form_logout').submit();
+    
+}
+
+function lister()
+{    
+    document.getElementById('form_listeFilm').submit();	
+}
